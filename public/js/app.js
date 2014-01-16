0a4e5192abe6493c7bd2b71c40569037cf2e1b3b
Adam Haley <adamhaley@gmail.com>
2014-01-15 22:51:50 -0800

    added tr hover color of blue, some bower bootstrap changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("jjgym", ['ngRoute','ngCookies','ui.calendar','ui.bootstrap.modal','jjgym.controllers']).config(function($routeProvider){
+var app = angular.module("jjgym", ['ngRoute','ngCookies','ui.calendar','ui.bootstrap.modal','ui.bootstrap.timepicker','jjgym.controllers']).config(function($routeProvider){
 	$routeProvider.when('/calendar', {
 		templateUrl: 'calendar.html',
 		controller: 'CalendarController'
