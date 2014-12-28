d1a6936b0f7eb971b6db25a696659d0e128be83a
Adam Haley <adamhaley@gmail.com>
2014-12-27 20:14:34 -0800

    Adjusted the colors on event add dialogue to match the color theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,62 +9,22 @@ module.exports = function(grunt) {
 					yuicompress: true
 				},
 				files: {
-					"./css/main.css": "./css/main.less"
+					"./public/css/main.css": "./public/css/main.less"
 				}
 			}
 		},
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: './css/',
-				src: ['main.css'],
-				dest: './css'
-			}
-		},
-		concat: {
-	      options: {
-	        separator: ';'
-	      },
-	      dist: {
-	        src: ['js/vendor/jquery/dist/jquery.js','js/vendor/bootstrap/js/modal.js','js/vendor/bootstrap/js/carousel.js','js/vendor/bootstrap/js/collapse.js','js/main.js'],
-	        dest: 'dist/<%= pkg.name %>.js'
-	      }
-	    },
-	    uglify: {
-	      options: {
-	        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-	      },
-	      dist: {
-	        files: {
-	          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-	        }
-	      }
-
-	    },
-    	jshint: {
-	      files: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js'],
-	      options: {
-	        // options here to override JSHint defaults
-	        globals: {
-	          jQuery: true,
-	          console: true,
-	          module: true,
-	          document: true
-	        }
-	      }
-	    },
 		// running `grunt watch` will watch for changes
 		watch: {
-			files: "./css/*.less",
+			files: "./public/css/*.less",
 			tasks: ["less"/*, "cssmin"*/]
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-contrib-jshint');
+	// grunt.loadNpmTasks('grunt-contrib-concat');
 
-	 grunt.registerTask('default', ["concat","uglify","cssmin"]);
+	 grunt.registerTask('default', ["less"]);
 
 };
