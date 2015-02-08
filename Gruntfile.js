bdd7d58a77a409d09d495aeba0a94210b31212fb
Adam Haley <adamhaley@gmail.com>
2015-02-08 12:51:06 -0800

    changed from less to stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,29 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		// running `grunt less` will compile once
 		pkg: grunt.file.readJSON("package.json"),
-		less: {
+		stylus: {
 			development: {
 				options: {
 					paths: ["./css"],
 					yuicompress: true
 				},
 				files: {
-					"./public/css/main.css": "./public/css/main.less"
+					"./public/css/main.css": "./public/css/main.styl"
 				}
 			}
 		},
 		// running `grunt watch` will watch for changes
 		watch: {
-			files: "./public/css/*.less",
-			tasks: ["less"/*, "cssmin"*/]
+			files: "./public/css/*.styl",
+			tasks: ["stylus"]
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
 	// grunt.loadNpmTasks('grunt-contrib-uglify');
 	// grunt.loadNpmTasks('grunt-contrib-jshint');
 	// grunt.loadNpmTasks('grunt-contrib-concat');
 
-	 grunt.registerTask('default', ["less"]);
+	 grunt.registerTask('default', ["stylus"]);
 
 };
