df4a288e1be353352aa4e931a23e8619510a75de
Adam Haley <adamhaley@gmail.com>
2015-02-14 19:00:56 -0800

    debugged the models a bit. crud routes are now working

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
-
+require('express-crud')(app);
+var User = require('../models/user')
 /*
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
-};
+app.crud('users', User);
+
+
+// exports.list = function(req, res){
+//   res.send("respond with a resource");
+// };
