f49eb0e27e89f8fce8339d37cfeb9a49fab2b3e5
Adam Haley <adamhaley@gmail.com>
2015-02-16 19:36:41 -0800

    attempt, not working yet

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,22 +2,42 @@ app.directive("textSlideshow",['$interval', function($interval){
 	return {
 		scope: {
 			data: "=data",
-			interval: "=interval"
+			interval: "=interval",
 		},
 		restrict: "C",
-		template: "<div ng-repeat=\"text in data\">{{text}}</div>",
-		link: function(scope, element, attrs){
-			delay = scope.interval * 1000;//seconds times 1000 to get ms
-			// $el = $('div', $(element)).first();
-			$interval(function(){
-				scope.data.push(scope.data.shift());
-				// $el.animate({left: '-100%'},200, function(){
-				// 	scope.data.push(scope.data.shift());
-				// 	$el.css({left: '100%'});
-				// 	$el.animate({left: 0},200);
-				// });
+		template: "<div ng-model=\"currentItem\" class=\"text-item animate-show\" ng-show=\"shown\" >{{currentItem}}</div>",
+		link: function(scope, element, attrs){	
+			function runInterval(){
+				scope.i = 0;
+				delay = scope.interval * 1000;//seconds times 1000 to get ms
+				scope.currentItem = scope.data[scope.i];
+				scope.shown = true;
+
+				$interval(function(){
+					if(scope.i == scope.data.length){
+						scope.i = 0;
+					}else{
+						scope.i++;
+					}	
+					scope.shown = false;
+					setTimeout(function(){scope.currentItem = scope.data[scope.i];scope.shown = true;},500);
 					
-			},delay);
+					// console.log(scope.i);
+					// scope.data.push(scope.data.shift());					
+				},delay);
+			}
+
+			started = false;
+			scope.$watch('data',function(newValue){
+				if(started){
+					return;
+				}else if(newValue == undefined){
+					return;
+				}else{
+					runInterval();
+					started = true;
+				}
+			});
 		}
 	}
 }]);
