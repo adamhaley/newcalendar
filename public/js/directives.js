3ecf3500b6f0678ac34f347907f2fb4b21a0881c
Adam Haley <adamhaley@gmail.com>
2015-02-16 17:45:36 -0800

    added ng-animate, some experiments with it, added symlink public fonts --> public/js/vendor/bootstrap/fonts

--- public/js/directives.js
+++ public/js/directives.js
@@ -8,8 +8,15 @@ app.directive("textSlideshow",['$interval', function($interval){
 		template: "<div ng-repeat=\"text in data\">{{text}}</div>",
 		link: function(scope, element, attrs){
 			delay = scope.interval * 1000;//seconds times 1000 to get ms
+			// $el = $('div', $(element)).first();
 			$interval(function(){
 				scope.data.push(scope.data.shift());
+				// $el.animate({left: '-100%'},200, function(){
+				// 	scope.data.push(scope.data.shift());
+				// 	$el.css({left: '100%'});
+				// 	$el.animate({left: 0},200);
+				// });
+					
 			},delay);
 		}
 	}
