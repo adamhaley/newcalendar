a5798f62522011c2dc1fa63bd8e702b00411a32b
Adam Haley <adamhaley@gmail.com>
2015-02-16 20:05:48 -0800

    jquery animate example, with annoying flicker happening

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,26 +1,31 @@
-app.directive("textSlideshow",['$interval', function($interval){
+app.directive("textSlideshow",['$interval', function($interval,$timeout){
 	return {
 		scope: {
 			data: "=data",
 			interval: "=interval",
 		},
 		restrict: "C",
-		template: "<div ng-model=\"currentItem\" class=\"text-item animate-show\" ng-show=\"shown\" >{{currentItem}}</div>",
+		template: "<div ng-model=\"currentItem\" class=\"text-item\">{{currentItem}}</div>",
 		link: function(scope, element, attrs){	
 			function runInterval(){
 				scope.i = 0;
 				delay = scope.interval * 1000;//seconds times 1000 to get ms
 				scope.currentItem = scope.data[scope.i];
-				scope.shown = true;
-
+			
 				$interval(function(){
 					if(scope.i == scope.data.length){
 						scope.i = 0;
 					}else{
 						scope.i++;
 					}	
-					scope.shown = false;
-					setTimeout(function(){scope.currentItem = scope.data[scope.i];scope.shown = true;},500);
+					$el = $(element).find('.text-item');
+					$el.
+					$el.animate({opacity: 0},200,function(){
+							
+							setTimeout(function(){scope.currentItem = scope.data[scope.i];$el.animate({opacity: 1},200);},100);
+						});
+
+					// $timeout(function(){scope.currentItem = scope.data[scope.i];scope.shown = true;},500);
 					
 					// console.log(scope.i);
 					// scope.data.push(scope.data.shift());					
