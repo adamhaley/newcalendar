039511b0a570ae930aec43d668bdbc0ece2c1dc9
Adam Haley <adamhaley@gmail.com>
2015-02-16 16:10:18 -0800

    text-slideshow successfully rotating text, styling on text-slideshow, moved directive styling to top of main.styl

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,12 +1,16 @@
-app.directive("textSlideshow",function($q, $interval){
+app.directive("textSlideshow",['$interval', function($interval){
 	return {
 		scope: {
-			data: "=data"
+			data: "=data",
+			interval: "=interval"
 		},
 		restrict: "C",
 		template: "<div ng-repeat=\"text in data\">{{text}}</div>",
 		link: function(scope, element, attrs){
-		
+			delay = scope.interval * 1000;//seconds times 1000 to get ms
+			$interval(function(){
+				scope.data.push(scope.data.shift());
+			},delay);
 		}
 	}
-});
+}]);
